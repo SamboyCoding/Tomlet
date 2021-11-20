610c34bfd8697c72b4b2babb5541cd9320d67ec9
Alexander Wulf <alex.f.wulf@t-online.de>
2021-11-20 12:40:39 +0100

    Serialization also works and tests added

---
+++ Tomlet.Tests/TestModelClasses/ComplexTestRecordWithAttributeMapping.cs
@@ -0,0 +1,15 @@
+﻿namespace Tomlet.Tests.TestModelClasses
+{
+    public record ComplexTestRecordWithAttributeMapping
+    {
+        [TomlProperty("string")]
+        public string MyString { get; init; }
+        public WidgetForThisComplexTestRecordWithAttributeMapping MyWidget { get; init; }
+    }
+
+    public record WidgetForThisComplexTestRecordWithAttributeMapping
+    {
+        [TomlProperty("my_int")]
+        public int MyInt { get; init; }
+    }
+}
