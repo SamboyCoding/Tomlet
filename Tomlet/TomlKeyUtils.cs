97002e3141c653b54bc3dc5df4be18891fe85e86
ITR <ITR@live.no>
2024-01-20 04:09:22 +0100

    Removed unused method

--- Tomlet/TomlKeyUtils.cs
+++ Tomlet/TomlKeyUtils.cs
@@ -7,31 +7,6 @@ namespace Tomlet
     internal static class TomlKeyUtils
     {
         private static readonly Regex UnquotedKeyRegex = new Regex("^[a-zA-Z0-9-_]+$");
-
-        private static int FindNextUnescapedQuote(string input, int startingIndex)
-        {
-            var i = startingIndex;
-            var isEscaped = false;
-            for (; i < input.Length; i++)
-            {
-                if (input[i] == '\\')
-                {
-                    isEscaped = !isEscaped;
-                    continue;
-                }
-
-                if (input[i] != '"' || isEscaped)
-                {
-                    isEscaped = false;
-                    continue;
-                }
-
-                return i;
-            }
-
-            return -1; // Return -1 if no unescaped quote is found
-        }
-
         internal static string FullStringToProperKey(string key)
         {
             var canBeUnquoted = UnquotedKeyRegex.Match(key).Success;
