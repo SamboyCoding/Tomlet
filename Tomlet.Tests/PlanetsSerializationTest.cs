SamboyCoding/Tomlet

commit 8dc1129531019ffa623b7c6b1250f522ed4c7135

fix(TomlTable): better key name checks (#33)

Tomlet/Models/TomlTable.cs
@@ -37,7 +37,7 @@ namespace Tomlet.Models
 
                 var builder = new StringBuilder("{ ");
 
-                builder.Append(string.Join(", ", Entries.Select(o => o.Key + " = " + o.Value.SerializedValue).ToArray()));
+                builder.Append(string.Join(", ", Entries.Select(o => EscapeKeyIfNeeded(o.Key) + " = " + o.Value.SerializedValue).ToArray()));
 
                 builder.Append(" }");
 
@@ -137,10 +137,8 @@ namespace Tomlet.Models
             builder.Append('\n');
         }
 
-        private string EscapeKeyIfNeeded(string key)
+        private static string EscapeKeyIfNeeded(string key)
         {
-            var didQuote = false;
-
             if (key.StartsWith("\"") && key.EndsWith("\"") && key.Count(c => c == '"') == 2)
                 //Already double quoted
                 return key;
@@ -149,18 +147,24 @@ namespace Tomlet.Models
                 //Already single quoted
                 return key;
 
-            if (key.Contains("\"") || key.Contains("'"))
+            if (IsValidKey(key))
+                return key;
+                    
+            key = TomlUtils.EscapeStringValue(key);
+            return TomlUtils.AddCorrectQuotes(key);
+        }
+
+        private static bool IsValidKey(string key)
+        {
+            foreach (var c in key)
             {
-                key = TomlUtils.AddCorrectQuotes(key);
-                didQuote = true;
+                if (!char.IsLetterOrDigit(c) && c != '_' && c != '-')
+                {
+                    return false;
+                }
             }
 
-            var escaped = TomlUtils.EscapeStringValue(key);
-
-            if (escaped.Contains(" ") || escaped.Contains("\\") && !didQuote)
-                escaped = TomlUtils.AddCorrectQuotes(escaped);
-
-            return escaped;
+            return true;
         }
 
         internal void ParserPutValue(string key, TomlValue value, int lineNumber)

Tomlet.Tests/DictionaryTests.cs
@@ -27,4 +27,30 @@ public class DictionaryTests
         //Just make sure this doesn't throw
         var serialized = TomletMain.TomlStringFrom(obj); 
     }
+
+    [Fact]
+    public void DictionaryKeysShouldBeProperlyEscaped()
+    {
+        var dictionary = new Dictionary<string, string>
+        {
+            {"normal-key", "normal-key"},
+            {"normal_key", "normal_key"},
+            {"normalkey", "normalkey"},
+            {"key with space", "\"key with spaces\""},
+            {"key!with{}(*%&)random[other+symbols", "\"key!with{}(*%&)random[other+symbols\""},
+            {"key/with/slashes", "\"key/with/slashes\""},
+            {"Nam\\e", "\"Nam\\\\e\""}
+        };
+
+        var obj = new ClassWithDictionary
+        {
+            GenericDictionary = dictionary
+        };
+        
+        var serialized = TomletMain.TomlStringFrom(obj);
+        foreach(var (_, expectedValue) in dictionary)
+        {
+            Assert.Contains(expectedValue, serialized);
+        }
+    }
 }

Tomlet.Tests/TableTests.cs
@@ -46,6 +46,26 @@ namespace Tomlet.Tests
             var tomlString = document.SerializedValue.Trim();
             Assert.Equal(inputString, tomlString);
         }
+        
+         [Theory]
+         [InlineData("normal-key", "normal-key")]
+         [InlineData("normal_key", "normal_key")]
+         [InlineData("normalkey", "normalkey")]
+         [InlineData("\"key with spaces\"", "\"key with spaces\"")]
+         [InlineData("key!with{}(*%&)random[other+symbols", "\"key!with{}(*%&)random[other+symbols\"")]
+         [InlineData("key/with/slashes", "\"key/with/slashes\"")]
+         [InlineData("Nam\\e", "\"Nam\\\\e\"")]
+         public void KeysShouldBeSerializedCorrectly(string inputKey, string expectedKey)
+         {
+             var inputString = $"""
+             ["Test Table"]
+             {inputKey} = "value"
+             """;
+             
+             var document = GetDocument(inputString);
+             var serializedDocument = document.SerializedValue.Trim();
+             Assert.Contains(expectedKey, serializedDocument);
+         }
 
         [Fact]
         public void TablesCanHaveQuotedDottedNames()

Tomlet.Tests/TestModelClasses/ClassWithDictionary.cs
@@ -5,4 +5,5 @@ namespace Tomlet.Tests.TestModelClasses;
 public class ClassWithDictionary
 {
     public Dictionary<string, Subname> name;
+    public Dictionary<string, string> GenericDictionary;
 }

commit 75d5c8f66461503eded6425c28005a20fe83dece

Fix keys being erroneously quoted sometimes
Caused by a regression in #33 that wasn't caught. Added a test to ensure it is caught in future.

Tomlet/Models/TomlTable.cs
@@ -158,7 +158,8 @@ namespace Tomlet.Models
         {
             foreach (var c in key)
             {
-                if (!char.IsLetterOrDigit(c) && c != '_' && c != '-')
+                //TODO Future: This check for period is perhaps not super valid but it was way more broken without it so I'm leaving it in for now.
+                if (!char.IsLetterOrDigit(c) && c != '_' && c != '-' && c != '.')
                 {
                     return false;
                 }

Tomlet.Tests/PlanetsSerializationTest.cs
@@ -1,0 +1,121 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Tomlet.Tests;
+
+public class PlanetsSerializationTest
+{
+    public class Pest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Pet
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Pest> Pests { get; set; }
+    }
+
+    public class Inhabitant
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Pet> Pets { get; set; }
+    }
+
+    public class Planet
+    {
+        public string Name { get; set; }
+        public List<Inhabitant> Inhabitants { get; set; }
+    }
+
+    public class Galaxy
+    {
+        public List<Planet> Planets { get; set; }
+    }
+
+    [Fact]
+    public void Test()
+    {
+        var milkyWay = new Galaxy
+        {
+            Planets = new()
+        };
+
+        var earth = new Planet
+        {
+            Name = "Earth",
+            Inhabitants = new()
+        };
+
+        var mammals = new Inhabitant
+        {
+            Name = "Humans",
+            Description = "xyz",
+            Pets = new()
+        };
+
+        var flea = new Pest
+        {
+            Name = "Fleas",
+            Description = "abc"
+        };
+
+        var dogs = new Pet
+        {
+            Name = "Dogs",
+            Description = "xyzzy",
+            Pests = new() { flea }
+        };
+
+        var cats = new Pet
+        {
+            Name = "Cats",
+            Description = "xyzzy",
+            Pests = new() { flea }
+        };
+
+        mammals.Pets.Add(dogs);
+        mammals.Pets.Add(cats);
+
+        earth.Inhabitants.Add(mammals);
+
+        milkyWay.Planets.Add(earth);
+
+        var mars = new Planet
+        {
+            Name = "Mars",
+            Inhabitants = new()
+        };
+
+        var grayOnes = new Inhabitant
+        {
+            Name = "Little Gray Men",
+            Description = "xyz",
+            Pets = new()
+        };
+
+        var martianBrainSlug = new Pet
+        {
+            Name = "Martian Brain Slug",
+            Description = "xyzzy"
+        };
+
+        grayOnes.Pets.Add(martianBrainSlug);
+
+        mars.Inhabitants.Add(grayOnes);
+
+        milkyWay.Planets.Add(mars);
+
+        var tomlString = TomletMain.TomlStringFrom(milkyWay);
+        
+        //Convert back to object. This should a) not throw due to broken keys, and b) have the same data.
+        var galaxy = TomletMain.To<Galaxy>(tomlString);
+        
+        Assert.Equal(milkyWay.Planets.Count, galaxy.Planets.Count);
+        Assert.Equal(milkyWay.Planets[0].Inhabitants.Count, galaxy.Planets[0].Inhabitants.Count);
+        Assert.Equal(milkyWay.Planets[0].Inhabitants[0].Pets.Count, galaxy.Planets[0].Inhabitants[0].Pets.Count);
+    } 
+}