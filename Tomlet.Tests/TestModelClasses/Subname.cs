0f83b66cf348e5e8ebac2bb12e8a998f330e33b6
ITR <ITR@live.no>
2025-02-26 17:49:52 +0100

    Fix enums used as table keys throwing exception (#53)

---
+++ Tomlet.Tests/TestModelClasses/Subname.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tomlet.Tests.TestModelClasses;
+
+public class Subname
+{
+    public string a;
+    public string b;
+
+    protected bool Equals(Subname other)
+    {
+        return a == other.a && b == other.b;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Subname)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(a, b);
+    }
+}
