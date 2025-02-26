182679c7dfa47e2489c4c8aa0ab2fcc31d8d9676
ITR <ITR@live.no>
2025-02-26 14:08:24 +0100

    Moved model classes for tests into TestModelClasses

--- Tomlet.Tests/TestModelClasses/Name.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tomlet.Tests.TestModelClasses;
-
-public class Subname
-{
-    public string a;
-    public string b;
-}
---
+++ Tomlet.Tests/TestModelClasses/Subname.cs
@@ -0,0 +1,6 @@
+namespace Tomlet.Tests.TestModelClasses;
+
+public class Subname
+{
+    public string a;
+    public string b;}
